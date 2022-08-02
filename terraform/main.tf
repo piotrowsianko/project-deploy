ac10a4973a9c906a82fabc3c7e41a821c31c0ed2
Piotr Owsianko <piotr.owsianko@globallogic.com>
2022-08-02 13:52:39 +0000

    Updated main.tf
    Former-commit-id: be9a677b4b217b1f5eb6038f3449b05be86690fb

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,3 +12,12 @@ provider "aws" {
 resource "aws_ecr_repository" "api_ecr" {
 	name = "repository-for-api"
 	}
+
+output "registry_id" {
+	description = "The account ID of the registry holding the repository"
+	value = aws_ecr_repository.repository.registry_id
+}
+output "repository_url" {
+	description = "The URL of the repository"
+	value = aws_ecr_repository.repository.repository_url
+}
