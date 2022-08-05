5bfb510e70d72447603eb801259a21d316be8c35
piotrowsianko <piotr.owsianko@globallogic.com>
2022-08-05 14:49:17 +0200

    Added deploy to production
    Former-commit-id: 3232e43e60e80e4ff0c5b10a022fc4ce9b5d9564

--- terraform/06-deploy-to-prod.tf
+++ terraform/06-deploy-to-prod.tf
@@ -5,11 +5,27 @@ resource "kubernetes_service" "deploy" {
   spec {
     type = "LoadBalancer"
     selector = {
-      app = "shop"
+      app = "website"
     }
     port {
       port        = 80
       target_port = 80
     }
   }
+}
+
+resource "kubernetes_pod" "production" {
+    count = var.prod ? 1 : 0
+    metadata {
+        name = "production"
+        labels = {
+            app = "website"
+        }
+    }
+    spec {
+        container {
+            name = "website"
+            image = "piotrowsianko/project:latest"
+        }
+    }
 }
