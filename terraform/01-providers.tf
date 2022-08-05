335d4ae3f583253c39d0b62058cc036de61f2094
piotrowsianko <piotr.owsianko@globallogic.com>
2022-08-05 17:10:13 +0200

    added kubernetes provider
    Former-commit-id: 471b1e82d31e9badb21b679b7b1436d64b024673

--- terraform/01-providers.tf
+++ terraform/01-providers.tf
@@ -20,3 +20,12 @@ provider "azurerm"{
   # Configure a specific Subscription ID (optional)
   subscription_id = "dfcefdc0-4ded-42a0-933b-100fbb1721ed"
 	}
+data "aws_eks_cluster" "eks" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
+}
