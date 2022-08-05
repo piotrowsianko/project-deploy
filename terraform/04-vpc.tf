75e513c16151fd5f8e12b13c8655b7a90ed02e87
piotrowsianko <piotr.owsianko@globallogic.com>
2022-08-05 13:43:04 +0200

    changed AZs
    Former-commit-id: d057e675f09e194e45fd751010d13ea24174a64d

--- terraform/04-vpc.tf
+++ terraform/04-vpc.tf
@@ -3,7 +3,7 @@ module "vpc" {
     version = "3.14.2"
     name = "production-vpc"
     cidr = "10.0.0.0/16"
-    azs = ["us-east-1"]
+    azs = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1e", "us-east-1f"]
     private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
     public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
     enable_nat_gateway   = true
