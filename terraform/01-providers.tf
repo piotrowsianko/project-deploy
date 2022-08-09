b13a3c84f097f2d7a654f7534d2f813a85f58d3c
Piotr Owsianko <piotr.owsianko@globallogic.com>
2022-08-09 07:54:27 +0000

    Updated 01-providers.tf
    Former-commit-id: 84be39a217bba60f07c82bbcc4e5857d29198391

--- terraform/01-providers.tf
+++ terraform/01-providers.tf
@@ -1,4 +1,11 @@
 terraform {
+	backend "s3" {
+    	bucket = "gl-piotrowsianko-project"
+    	key    = "production/terraform.tfstate"
+    	region = "us-east-1"
+  	}
+	
+	
 	required_providers {
 		aws = {
 			source = "hashicorp/aws"
