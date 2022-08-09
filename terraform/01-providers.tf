d132b567a27367f2e74f84565034481b5d0bb1f7
piotrowsianko <piotr.owsianko@globallogic.com>
2022-08-09 14:06:58 +0200

    Added terraform state file to s3 bucket
    Former-commit-id: 58042e920adc4596171e59a70497b5e218862a94

--- terraform/01-providers.tf
+++ terraform/01-providers.tf
@@ -1,4 +1,9 @@
 terraform {
+	backend "s3" {
+		bucket = "gl-piotrowsianko-projectapp"
+		key = "production/terraform.tfstate"
+		region = "us-east-1"
+	}
 	required_providers {
 		aws = {
 			source = "hashicorp/aws"
