9a66aa7d3a054183a163dbf11dec3b3f8b5fa8ea
piotr.owsianko <piotr.owsianko@globallogic.com>
2022-08-12 23:20:24 +0200

    Changes in pipelines
    Former-commit-id: e8892da0e72ee024582ee8b900d27bd1aa9a5aee

---
+++ terraform/07-deploy-to-prod.tf
@@ -0,0 +1,39 @@
+//  deploy website on a public endpoint
+resource "kubernetes_service" "deploy" {
+  metadata {
+    name = "deploy"
+  }
+  spec {
+    type = "LoadBalancer"
+    selector = {
+      app = "website"
+    }
+    port {
+      port        = 80
+      target_port = 80
+    }
+  }
+}
+resource "local_file" "endpoint" {
+  content  = kubernetes_service.deploy.status[0].load_balancer[0].ingress[0].hostname
+  filename = ".endpoint"
+}
+
+resource "kubernetes_pod" "prodenv" {
+  count = var.prod ? 1 : 0
+  depends_on = [
+    module.eks
+  ]
+  metadata {
+    name = "prodenv"
+    labels = {
+      app = "website"
+    }
+  }
+  spec {
+    container {
+      name  = "website"
+      image = "piotrowsianko/project:stable"
+    }
+  }
+}
