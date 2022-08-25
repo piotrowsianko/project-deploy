d1ab46541a1cb80f4b806c11dbad07f9f6e5de5d
piotr.owsianko <piotr.owsianko@globallogic.com>
2022-08-25 09:26:59 +0200

    changed database.py to work with url from file
    Former-commit-id: 2c4c4b62db834b7555f8f231de400f124fa87685

--- app/database.py
+++ app/database.py
@@ -2,17 +2,17 @@ from requests import session
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from data import url
 
 
 
 
-#SQLALCHEMY_DATABASE_URL = database_address
 #SQLALCHEMY_DATABASE_URL = "postgresql://"+ os.getenv['db_username']+ ":" +os.getenv['db_password']+"@"+ os.getenv['db_endpoint'] +"/postgres"
 #SQLALCHEMY_DATABASE_URL = "postgresql:/$(db_username):$(db_password)@$(db_endpoint)/postgres"
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:password1@terraform-2022082407533712560000000b.c0zbwl7kvrhj.us-east-1.rds.amazonaws.com:5432/postgres"
-
+#SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:password1@terraform-2022082407533712560000000b.c0zbwl7kvrhj.us-east-1.rds.amazonaws.com:5432/postgres"
+SQLALCHEMY_DATABASE_URL = url
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
