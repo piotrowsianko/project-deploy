ab6f52dc28793752c728bfc9cfef3f823269b100
piotr.owsianko <piotr.owsianko@globallogic.com>
2022-08-25 09:12:23 +0200

    update of kube.ctl and db_sg
    Former-commit-id: 368b8491b7e86b6c8cb5a2bdfbb0b2b50a32576a

--- app/database.py
+++ app/database.py
@@ -9,12 +9,13 @@ from sqlalchemy.orm import sessionmaker
 #SQLALCHEMY_DATABASE_URL = database_address
 #SQLALCHEMY_DATABASE_URL = "postgresql://"+ os.getenv['db_username']+ ":" +os.getenv['db_password']+"@"+ os.getenv['db_endpoint'] +"/postgres"
 #SQLALCHEMY_DATABASE_URL = "postgresql:/$(db_username):$(db_password)@$(db_endpoint)/postgres"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password1@terraform-2022081913233896190000000b.c1pyjveipgev.us-east-1.rds.amazonaws.com:5432/postgres"
+
+
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:password1@terraform-2022082407533712560000000b.c0zbwl7kvrhj.us-east-1.rds.amazonaws.com:5432/postgres"
 
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-connection = engine.connect()
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
