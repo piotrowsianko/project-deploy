71753625af964f7510e5652ecd684cbd9e8b8716
piotr.owsianko <piotr.owsianko@globallogic.com>
2022-08-29 12:09:19 +0200

    Changes in outputs
    Former-commit-id: 17296efc7bd61cb6236a3f70d256d5ee5e171272

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -13,12 +13,12 @@ output "db_endpoint" {
 
 }
 
-output "PROD_REPO_URL" {
+output "TESTING_REPO_URL" {
 	description = "The URL of the repository"
 	value = aws_ecr_repository.testing_api_ecr.repository_url
 }
 
-output "db_endpoint" {
+output "db_testing_endpoint" {
 	description = "Endpoint URL of the database"
 	value = aws_db_instance.pioows-testing-db.endpoint
 
